abf167ab697faa45b4333348b0cb98ba58f64065
Joao Paulo <jaozindasilva007@gmail.com>
2026-05-20 22:07:47 -0300

    progress: myLibrary 2/3 concluído

--- script.js
+++ script.js
@@ -8,21 +8,20 @@ const dataApi = async () => {
         load.classList.remove('hide')
 
         const resposta = await fetch('https://gutendex.com/books')
+
         const data = await resposta.json()
-
-
         const infoLivros = data.results.map(el => {
             return {
                 titulo: el.title,
                 capaImg: el.formats["image/jpeg"],
                 subTitulo: el.summaries,
 
-                autor: el.authors[0].name,
+                autor: el.authors[0].name.split(', ')[0],
                 lingua: el.languages,
 
                 livro: el.formats["text/plain"],
                 direitos: el.copyright,
-                ficcao: el.subjects.join(', '),
+                ficcao: el.bookshelves.join(' '),
 
                 download: el.download_count
             }
@@ -36,15 +35,17 @@ const dataApi = async () => {
         document.body.classList.remove('ativeBody')
         document.querySelector('.cLoad').classList.add('hide')
 
+        /* fazer um tratamento como uma pagina de erro 404 */
+        /* fazer um tratamento de um msg se estiver demorando para carregar */
     }
-
 }
 
 
 const addLivros = async () => {
 
-
     const dadosApi = await dataApi()
+    console.log('execultou')
+    const todosGeneros = [];
 
     dadosApi.slice(0, 3)
         .forEach((el, index) => {
@@ -74,17 +75,26 @@ const addLivros = async () => {
 
     dadosApi.forEach((el, index) => {
 
-        const listaLivros = document.getElementById('listaLivros')
+        const generoLivro = el.ficcao
+        const generosLivros = guardarGenero(generoLivro).join(' ')
 
-        listaLivros.innerHTML += `
-            <div class="livro" data-index="${index}">
+        const listaLivros = document.getElementById('listaLivros')
+        const containerLivros = document.querySelector('.containerLivros')
+
+
+        const livroAll = `
+            <div class="livro" data-index="${index}" data-genero="${generosLivros}" data-author="${el.autor}">
                 <img src="${el.capaImg}" alt="Capa do livro" class="imgDoLivro">
                 <div class="cLivroInfo">
                     <h3>${el.titulo}</h3>
-                    <small>${el.autor}</small>                
+                    <small class="autorSub">${el.autor}</small>                
                     <p>${el.subTitulo}</p>        
                 </div>
             </div>`
+
+        listaLivros.innerHTML += livroAll
+        containerLivros.innerHTML += livroAll
+
 
         const allLivro = document.querySelectorAll('.livro')
         allLivro.forEach((el) => {
@@ -97,9 +107,10 @@ const addLivros = async () => {
         })
 
     })
-
 }
+
 addLivros()
+
 
 
 const livrosHide = (item, dadosApi) => {
@@ -135,6 +146,7 @@ const livrosHide = (item, dadosApi) => {
         behavior: 'smooth',
         block: 'start'
     });
+
 }
 
 
@@ -180,6 +192,267 @@ const fecharLeitura = () => {
     document.querySelector('.sectionLivros').classList.remove('hide')
 }
 
-/* fazer um innerHtml pra colocar todas as clasees duma vez para nao ficar precisando ficar criarnado varioas consts  e almentando o codigo */
 
-/* ajustar pra criar um function pra criar os livros com hide */
+
+
+const tiposGeneros = async () => {
+    /* filtros */
+    const dadosApiGenero = await dataApi()
+    const todosGeneros = [];
+
+    dadosApiGenero.forEach((el) => {
+        const generoLivro = el.ficcao
+        const generosLivros = guardarGenero(generoLivro)
+        todosGeneros.push(...generosLivros)
+    })
+
+    const generosFiltrados = [... new Set(todosGeneros)]
+
+    generosFiltrados.forEach((el) => {
+        const containerFiltroGenero = document.querySelector('.containerFiltroGenero');
+
+        containerFiltroGenero.innerHTML += `
+            <div class="cardGenero" data-genero="${el}">
+                ${el}
+            </div>`;
+    })
+
+    /* autor */
+
+    dadosApiGenero.slice(0, 5).forEach((el) => {
+        const containerAutores = document.querySelector('.containerAutores');
+
+        containerAutores.innerHTML += `
+        <div class="cardAutor" data-author="${el.autor}">
+            ${el.autor}
+        </div>`;
+    })
+}
+
+const filterBtns = () => {
+    /* btn close */
+    const filtroHp2 = document.querySelector('.filtroHp2').addEventListener('click', () => {
+        document.querySelectorAll('.livro').forEach(e => e.classList.remove('hide'))
+
+        cardGenero.forEach(e => e.classList.remove('ativo'))
+        cardAutor.forEach(e => e.classList.remove('ativo'))
+    })
+
+    /* input */
+    const inputCategoria = document.querySelector('.inputCategoria').addEventListener('input', (evt) => {
+        const inputNameLivro = evt.target.value.trim().toLowerCase()
+        let encontrados = 0;
+
+        document.querySelectorAll('.livro').forEach((e) => {
+            const nomeLivro = e.querySelector('.cLivroInfo h3').textContent.toLowerCase()
+            const nomeAutor = e.querySelector('.autorSub').textContent.toLowerCase()
+            const nomeGenero = e.dataset.genero.toLowerCase()
+
+            const encontrou =
+                nomeLivro.includes(inputNameLivro) ||
+                nomeAutor.includes(inputNameLivro) ||
+                nomeGenero.includes(inputNameLivro);
+
+            e.classList.toggle('hide', !encontrou)
+
+            if (encontrou) encontrados++;
+            const estadoVazio = document.querySelector('.estadoVazio')
+            if (encontrados === 0) {
+                estadoVazio.classList.remove('hide')
+            } else {
+                estadoVazio.classList.add('hide')
+            }
+        })
+    })
+
+
+    const cardGenero = document.querySelectorAll('.cardGenero')
+
+    cardGenero.forEach((el) => {
+
+        el.addEventListener('click', (evt) => {
+            cardGenero.forEach(e => e.classList.remove('ativo'))
+            evt.target.classList.toggle('ativo')
+
+            const generoTipo = evt.target.dataset.genero
+            const livroGenero = document.querySelectorAll('.livro')
+
+            livroGenero.forEach((el) => {
+                const dataGenero = el.dataset.genero.split(' ')
+
+                if (dataGenero.includes(generoTipo)) {
+                    cardAutor.forEach(e => e.classList.remove('ativo'))
+                    el.classList.remove('hide')
+
+                } else {
+                    el.classList.add('hide')
+                }
+            })
+
+
+        })
+    })
+
+
+    const cardAutor = document.querySelectorAll('.cardAutor')
+    cardAutor.forEach((el) => {
+        el.addEventListener('click', (evt) => {
+            cardAutor.forEach(e => e.classList.remove('ativo'))
+            evt.target.classList.add('ativo')
+
+            const authorTipo = evt.target.dataset.author
+
+            const livroGenero = document.querySelectorAll('.livro')
+            livroGenero.forEach((el) => {
+                const dataAuthor = el.dataset.author
+
+                if (dataAuthor.includes(authorTipo)) {
+                    cardGenero.forEach(e => e.classList.remove('ativo'))
+                    el.classList.remove('hide')
+
+                } else {
+                    el.classList.add('hide')
+                }
+            })
+
+        })
+    })
+
+}
+
+const btnsHomePage = () => {
+
+    document.getElementById('btnExplorar').addEventListener('click', () => {
+        document.getElementById('sectionLivros').scrollIntoView('#sectionLivros')
+    })
+
+    document.querySelectorAll('.btnCategorias').forEach((el) => {
+        el.addEventListener('click', () => {
+            document.querySelectorAll('.navItem').forEach(e => e.classList.remove('active')
+            )
+
+            document.querySelector('.btnCategoriasNav').classList.add('active')
+            document.getElementById('sectionLivros').classList.add('hide')
+            document.querySelector('.hero').classList.add('hide')
+            document.querySelector('.categoriasSection').classList.remove('hide')
+        })
+    })
+
+    document.querySelectorAll('.homeNav').forEach((el) => {
+        el.addEventListener('click', (evt) => {
+            document.querySelectorAll('.navItem').forEach(e => e.classList.remove('active')
+            )
+            document.querySelector('.homeNavHero').classList.add('active')
+            document.getElementById('sectionLivros').classList.remove('hide')
+            document.querySelector('.hero').classList.remove('hide')
+            document.querySelector('.categoriasSection').classList.add('hide')
+        })
+    })
+}
+btnsHomePage()
+
+
+const esperaInit = async () => {
+    await tiposGeneros()
+    filterBtns()
+}
+
+esperaInit()
+
+const guardarGenero = (generosTextos) => {
+
+    const infosGenero = generosTextos.toLowerCase();
+
+    const generos = [];
+
+    if (infosGenero.includes("fantasy"))
+        generos.push("Fantasy");
+
+    if (infosGenero.includes("science"))
+        generos.push("Science");
+
+    if (infosGenero.includes("history"))
+        generos.push("History");
+
+    if (
+        infosGenero.includes("romance") ||
+        infosGenero.includes("love")
+    )
+        generos.push("Romance");
+
+    if (infosGenero.includes("adventure"))
+        generos.push("Adventure");
+
+    if (
+        infosGenero.includes("children") ||
+        infosGenero.includes("juvenile")
+    )
+        generos.push("Children");
+
+    if (
+        infosGenero.includes("horror") ||
+        infosGenero.includes("gothic")
+    )
+        generos.push("Horror");
+
+    if (
+        infosGenero.includes("mystery") ||
+        infosGenero.includes("detective")
+    )
+        generos.push("Mystery");
+
+    if (infosGenero.includes("poetry"))
+        generos.push("Poetry");
+
+    if (infosGenero.includes("drama"))
+        generos.push("Drama");
+
+    if (infosGenero.includes("philosophy"))
+        generos.push("Philosophy");
+
+    if (infosGenero.includes("religion"))
+        generos.push("Religion");
+
+    if (
+        infosGenero.includes("biography") ||
+        infosGenero.includes("autobiography")
+    )
+        generos.push("Biography");
+
+    if (
+        infosGenero.includes("politics") ||
+        infosGenero.includes("government")
+    )
+        generos.push("Politics");
+
+    if (
+        infosGenero.includes("education") ||
+        infosGenero.includes("teaching")
+    )
+        generos.push("Education");
+
+    if (
+        infosGenero.includes("art") ||
+        infosGenero.includes("painting")
+    )
+        generos.push("Art");
+
+    if (infosGenero.includes("music"))
+        generos.push("Music");
+
+    if (
+        infosGenero.includes("travel") ||
+        infosGenero.includes("journey")
+    )
+        generos.push("Travel");
+
+    if (
+        infosGenero.includes("nature") ||
+        infosGenero.includes("animals")
+    )
+        generos.push("Nature");
+
+    return generos.length ? generos : ["Fiction"];
+};
+
+
